dea9aaf887390cf4e5107920e8aabb28d9a172c0
codevic <vaibhavsoni.pesit@gmail.com>
2019-06-05 00:27:15 -0500

    faster and cleaner code with async and await

---
+++ Weather/Model/WeatherInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace Weather
+{
+    //data structure for weather info
+    class WeatherInfo
+    {
+        public Weather[] weather { get; set; }
+        public Main main { get; set; }
+        public string name { get; set; }
+
+    }
+    public class Main
+    {
+        public float temp { get; set; }
+        public float pressure { get; set; }
+        public int humidity { get; set; }
+        public float temp_min { get; set; }
+        public float temp_max { get; set; }
+    }
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+}
---
+++ Weather/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace Weather
+{
+    class Program
+    {
+        //cities for reporting weather
+        static private string[] cities = { "Houston", "Austin", "Dallas", "San Antonio" };
+        //cache memory for weather
+        static private string[] cache;
+
+        //cache function for weather
+        static protected void CacheWeather(Object stateInfo)
+        {
+            //Console.WriteLine("Cache updated, timer working"); only to test
+            //create weather fetcher instance
+            WeatherFetcher weather = new WeatherFetcher();
+            int i;
+            //for getting all cities weather from web api
+            for (i = 0; i < cities.Length; i++)
+            {
+                //get weather info from weather web api
+                cache[i] = weather.GetCityWeather(cities[i]);
+            }
+        }
+
+        //display weather info
+        static private void DisplayWeather(WeatherInfo info)
+        {
+            Console.WriteLine($"City: {info.name}");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine($"Weather: {info.weather[0].main} ({info.weather[0].description})");
+            Console.WriteLine($"temperature: {ConvertToFahrenheit(info.main.temp)}" + "F");
+            Console.WriteLine($"pressure: {info.main.pressure}");
+            Console.WriteLine("humidity: " + info.main.humidity + "%\n");
+            
+
+        }
+
+         static void Main(string[] args)
+        {
+            cache = new string[4];
+            //fetcher weather into cache
+            CacheWeather(null);
+            bool quit = false;  //if quit is true,main progarm is killed and thread is terminated
+
+            //set time interval with 20s
+            TimerCallback interval = new TimerCallback(CacheWeather);
+            Timer timer = new Timer(interval, null, 0, 20000);
+
+            
+            int i = 0;
+
+
+            DeserializeAndDisplay(cache[i]);
+            i++;
+
+            //loop
+            while (!quit)
+            {
+                //check the input key
+                //      string userMenuChoice = Console.ReadLine();
+                ConsoleKeyInfo keyinfo = Console.ReadKey();
+                switch (keyinfo.Key)
+                {
+                    //if key is Enter,next city displayed
+                    case ConsoleKey.Spacebar:
+                        //get weather info from cache and display
+                        DeserializeAndDisplay(cache[i]);
+                                          
+                        //prepare for next city
+                        i = (i + 1) % cities.Length;
+                        break;
+                    //if key is Escape,exit program
+                    case ConsoleKey.Escape:
+                        quit = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You must enter 'Space' or 'Escape' to quit");
+                        break;
+                }
+            }
+
+
+
+        }
+
+        public static double ConvertToFahrenheit(float degreesKelvin)
+        {
+            return (degreesKelvin - 273.15) * 1.8 + 32;
+        }
+
+         public static void DeserializeAndDisplay(string inffo)
+        {
+            //get weather info from cache
+            //weather info instance
+            WeatherInfo info = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherInfo>(inffo);
+            //display weather info for each city
+            DisplayWeather(info);
+            
+            
+        }
+    }
+}
